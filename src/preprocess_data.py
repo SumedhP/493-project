c60cf5606bfaf0b24fc8bf9a5a04d3188277f520
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-05-07 21:07:21 -0700

    dataprocessing changes

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 pids = [
     "BK7610",
@@ -23,51 +24,67 @@ acc_data = pd.read_csv(ACCEL_DATA_FILE)
 all_combined = []
 
 for pid in pids:
+    print()
     print(f"Looking at pid {pid}")
     PID_TAC_FILE = f"data/clean_tac/{pid}_clean_TAC.csv"
     tac_data = pd.read_csv(PID_TAC_FILE)
-    
+
     """
     Example data:
     1493737156832,BK7610,-0.1353,0.2467,0.1022
     1493737156859,BK7610,-0.0811,0.3475,0.162
     """
-    
+
     pid_acc_data = acc_data[acc_data["pid"] == pid]
     time = pid_acc_data["time"]
-    # print("Starting time: ", min(time))
-    # print("Ending time: ", max(time))
-    # print("Duration (ms): ", max(time) - min(time))
+
+    # Get rid of duplicate timestamp values
+    dupes = pid_acc_data["time"].duplicated().sum()
+    print(f"PID {pid} has {dupes} exact-duplicate timestamps")
+    pid_acc_data = pid_acc_data.drop_duplicates("time")
     
-    # print("Overall have datapoints: ", len(time))
-    # print("Expected at 40hz: ", (max(time) - min(time)) / 1000 * 40)
-    
+    modified_acc_data = pid_acc_data.set_index("time")
+    modified_acc_data = modified_acc_data.drop(columns=["pid"])
+
+    new_index = np.arange(pid_acc_data["time"].min(), pid_acc_data["time"].max(), 25)
+
+    resampled_acc_data = (
+        modified_acc_data.reindex(new_index)
+        .infer_objects(copy=False)
+        .interpolate()  # Make up data wahoo
+        .ffill()  # carries first real value backward to start
+        .bfill()  # carries last real value forward to end
+    )
+    resampled_acc_data = resampled_acc_data.reset_index().rename(columns={"index": "time"})
+    resampled_acc_data["pid"] = pid
+
+    resampled_acc_data["second"] = resampled_acc_data["time"] // 1000
+    counts = resampled_acc_data.groupby("second").size()
+    valid_seconds = counts[counts == 40].index
+    resampled_acc_data = resampled_acc_data[resampled_acc_data["second"].isin(valid_seconds)].drop(
+        columns=["second"]
+    )
+
     tac_data["timestamp"] = tac_data["timestamp"] * 1000
-    
-    # tac_time, tac_bac = tac_data["timestamp"], tac_data["TAC_reading"]
-    # tac_time *= 1000 # Converts from s to ms to match accel data
-    
+
     # LEFT OUTER JOIN TIME
     merged = pd.merge_asof(
-        pid_acc_data,
+        resampled_acc_data,
         tac_data,
         left_on="time",
         right_on="timestamp",
         direction="backward",
-        suffixes=("", "_tac")
+        suffixes=("", "_tac"),
     )
-    
+
     merged["intoxicated"] = (merged["TAC_Reading"] > 0.08).astype(int)
-    # print(merged["intoxicated"].mean())
-    # print(merged)
-    
-    cleaned = merged[["time", "pid", "x", "y", "z", "intoxicated"]]
+
+    cleaned = merged[["time", "pid", "x", "y", "z", "intoxicated"]].copy()
     
     all_combined.append(cleaned)
-    
+
 final_df = pd.concat(all_combined, ignore_index=True)
 final_df.to_csv("combined_data.csv", index=False)
 
 print(final_df)
 print(final_df["intoxicated"].mean())
-
